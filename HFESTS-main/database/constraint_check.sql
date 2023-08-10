870b976464fd5a4bb5d2c0d7b5d100a44748d5af
Mojtaba Peyrovian <mojtaba.peyrovian@gmail.com>
2023-08-10 13:53:46 -0400

    Delete HFESTS-main directory

--- HFESTS-main/database/constraint_check.sql
+++
@@ -1,9 +0,0 @@
-
-ALTER TABLE `mydb`.`employment`
-    ADD CONSTRAINT `CHK_employment_date_range`
-        CHECK (`endDate` >= `startDate`);
-
-ALTER TABLE `mydb`.`schedule`
-    ADD CONSTRAINT `CHK_schedule_date_range`
-        CHECK (`endTime` >= `startTime`);
-
